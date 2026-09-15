ba8b52da3509aacbc7938bffbb9c385e116387b4
Gaby <gabrielyfdreis@gmail.com>
2026-09-15 11:38:45 -0300

    adicionada pasta 15-09 - variaveis

---
+++ 15-09/script.js
@@ -0,0 +1,40 @@
+//let miranha = "Peter Parker";
+// let numero = 999999999;
+// let numero2 = "999999999";
+//let idade = 17;
+//var nome = "Gaby";
+//nome = "Pedro"
+
+//const nome = "Gaby"
+//nome = "Pedro"
+
+//idade = 20;
+//idade = 21;
+
+//console.log("O miranha é o " + miranha + " e ele tem " + idade + " anos");
+// Crie 4 variaveis contendo: nome, sobrenome, idade e curso 
+// Exiba no console em uma unica frase
+
+let nome = "Gabriely";
+let sobrenome = " Reis";
+let idade = 17;
+let curso = "JavaScript"
+
+// console.log("Meu nome é " + nome + sobrenome + ", tenho " + idade + " anos" + " e faço curso de " + curso)
+
+// let nome = "Gaby"; //string
+// let peso = 55; //int
+// let altura = 1.60; //float
+// let funcionário = true; //bool - tru == verdadeiro ou false == falso
+
+// let cidade = null;
+// let estado;
+// console.log(typeof nome);
+// console.log(typeof peso);
+// console.log(typeof altura);
+// console.log(typeof funcionário);
+
+console.log("A variavel nome tem " + nome + " e é do tipo " + typeof nome);
+console.log("A variavel sobrenome tem " + sobrenome + " e é do tipo " + typeof sobrenome);
+console.log("A variavel idade tem " + idade + " e é do tipo " + typeof idade);
+console.log("A variavel curso tem " + curso + " e é do tipo " + typeof curso);
