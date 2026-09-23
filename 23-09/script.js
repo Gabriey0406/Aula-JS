32ffaa1526ae58538e0988f9e172a8ad1d6ff7bc
Gaby <gabrielyfdreis@gmail.com>
2026-09-23 11:54:38 -0300

    aula 23-09 - estrutura de repetição

---
+++ 23-09/script.js
@@ -0,0 +1,110 @@
+// ESTRUTURA DE REPETIÇÃO 
+//for
+// for (variavel; condicao; incremento) {
+    
+    
+// }
+
+// for (let index = 0; index < 10; index++) {
+//     console.log(index);
+    
+// }
+
+// DECREMENTO 
+// for (let index = 10; index >= 0; index--) {
+//     console.log(index);
+    
+// }
+
+// //INCREMENTO PERSONALIZADO
+// for (let index = 10; index >= 0; index-=2) {
+//     console.log(index);
+//     ;
+    
+// }
+
+// for (let index = 1; index <= 20; index++) {
+//    console.log(index);
+//    ;
+    
+// }
+// for (let index =0; index <= 50; index +=5) {
+//    console.log(index);
+//    ;
+    
+// }
+// for (let index = 10; index >= 0; index--){
+//    console.log(index);
+   
+   
+// }
+// console.log("Contagem finalizada");
+
+//     WHILE
+// let contador = 0;
+// while (contador <= 10) {
+    
+//     console.log(contador);
+    
+//     contador++;
+// }
+// let contador = 10
+// while (contador <= 20) {
+//     console.log(contador);
+//     contador++;
+// }
+
+// let contador = 2
+// while (contador <= 20) {
+//     console.log(contador);
+//     contador+=2;
+// }
+
+//    DO...WHILE
+// let n = 10;
+// while (n < 5) {
+//     console.log("WHILE");
+    
+// }
+
+
+// do {
+//     console.log("DO...WHILE");
+    
+// } while (n < 5);
+
+// let contador = 0;
+// do {
+//     console.log(contador);
+//     contador++;
+// } while (contador <= 10);
+
+// let contador = 10
+// do {
+//     console.log(contador);
+//     contador++;
+
+// } while (contador <=100);
+
+// let contagem = 10
+// do {
+//     console.log(contagem);
+//     contagem--;
+// } while (contagem>= 1);
+
+// Array(LISTA)
+let nome1 = "Levi";
+let nome2 = "Duda";
+let nome3 = "Gustavo";
+let nome4 = "Bernardo";
+
+let nomes = ["Levi", "DUDA","Gustavo", "Bernardo"];
+// console.log(nomes);
+console.log(nomes[1]);
+
+nome4 = "Ana";
+nomes[3]="Ana"
+console.log(nomes[3]);
+
+console.log(nomes.length);
+
